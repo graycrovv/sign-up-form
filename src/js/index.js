1fd789e6d7bd12e0ec6e94e10a8ba74fb89da35f
Alina Pohl <alinaepohl@gmail.com>
2023-07-10 16:50:44 +0200

    Optimize the functions.
    Make the visual validation functionality work properly.
    Check class names to avoid diffrent spelling (ex. "phone" vs.
    "telephone").

--- src/js/index.js
+++ src/js/index.js
@@ -64,13 +64,14 @@ const password = document.querySelector('input[name="password"]');
 const passwordConfirmation = document.querySelector(
   'input[name="password-confirm"]'
 );
-const phoneXMark = document.querySelector(".phone-number .fa-xmark");
+const telephoneXMark = document.querySelector(".telephone .fa-xmark");
 const passwordXMark = document.querySelector(".password .fa-xmark");
 const confirmationXMark = document.querySelector(".password-confirm .fa-xmark");
 const patterns = {
   telephone: /^[0-9]{9}$/,
-  password: new RegExp(/^[a-z]{8,}$/),
+  password: new RegExp(/^^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/),
 };
+
 const changeToXMark = (icon) => {
   icon.classList.add("fa-xmark");
   icon.classList.remove("fa-check");
@@ -93,8 +94,24 @@ const checkPasswordMatch = () => {
   }
 };
 const checkValidation = (value, input) => {
+  console.log(value);
   console.log(patterns[value].test(input));
+  if (value === "telephone") {
+    if (patterns[value].test(input)) {
+      changeToCheckedMark(telephoneXMark);
+    } else {
+      changeToXMark(telephoneXMark);
+    }
+  }
+  if (value === "password") {
+    if (patterns[value].test(input)) {
+      changeToCheckedMark(passwordXMark);
+    } else {
+      changeToXMark(passwordXMark);
+    }
+  }
 };
+//try to write validation that operates on "general" vars, so it's less code (one if instead of seperate ones for password and phone num)
 inputs.forEach((input) => {
   input.addEventListener("input", (e) => {
     inputName = e.target.attributes.name.value;
